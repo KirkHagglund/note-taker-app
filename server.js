eaf843d245901a0f9acee894866972cb728e4b20
elkirko <kirkhagglund@gmail.com>
2023-02-22 08:46:02 -0600

    Changed routes

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.use(express.static('public'));
 // Create express requests
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.get('/notes/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+    res.sendFile(path.join(__dirname, '/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
